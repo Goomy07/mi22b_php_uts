46b100c431927114921fa35db6dde867c363240e
Goomy07 <159118940+Goomy07@users.noreply.github.com>
2024-07-24 12:37:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
+    <ul id="menu">
+        <li><a href="index.php">Beranda</a></li>
+        <li><a href="fahmi_login.php">Login</a></li>
+        <li><a href="fahmi_loop.php">Loop</a></li>
+        <li><a href="fahmi_profil.php">Profil</a></li>
+    </ul>
+</body>
+</html>
+<style>
+    #menu{
+        margin: 15;
+        padding: 50;
+    }
+    #menu a {
+        text-decoration: none;
+    }
+    #menu li {
+    display: inline-block;
+    border: 1px solid gray;
+    padding: 20px;
+    
+    }
+    #menu li:hover {
+        background-color: yellowgreen;
+    }
+</style>
